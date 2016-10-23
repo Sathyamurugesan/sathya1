1b6ad45870a4c2975218163324cd207239682eb8
Sathyamurugesan <sathiya8297@gmail.com>
2016-10-24 02:10:33 +0530

    Create armst1.c

---
+++ armst1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int r1,r2,t,num,i;
+printf("Enter the range:");
+scanf("%d %d",&r1,&r2);
+printf("The armstrong number between %d and %d are",r1,r2);
+ for(i=r1+1;i<n2;++i) {
+    t=i;
+    num=0;
+      while(t!=0) {
+        r=t%10;
+        num=num+r*r*r;
+        t=t/10;
+    }
+    if(i==num) {
+      printf("%d",i);
+      }
+    }
+   getch();
+}
