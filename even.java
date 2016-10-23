e4612640a0fc3f3770e648afa5c414fa5c0d789f
Sathyamurugesan <sathiya8297@gmail.com>
2016-10-24 01:20:36 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,14 @@
+class Even {
+  public class void main(String [] args) {
+    int range;
+      Scanner s = new Scanner(System.in);
+      System.out.println("Enter the range:");
+      range = s.nextInt();
+      System.out.println("The even numbers between two intervals" + range);
+        for(i=0;i<=range;i++) {
+           if(i%2==0) {
+             System.out.println(i + "");
+             }
+           }
+     }
+}
